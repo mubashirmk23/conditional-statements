2ef6e30271f119444417ba7e7e2e5815d3e906cc
mubashir <mubashirshamsudheen@gmail.com>
2024-08-20 15:54:48 +0530

    conditional

---
+++ prgrm37.dart
@@ -0,0 +1,12 @@
+void main(){
+
+int age = 21;
+
+if (age>18){
+print("you are elgible to voter");
+}
+else {
+print ("you are not elgible to voter");
+
+}
+}
---
+++ prgrm38.dart
@@ -0,0 +1,32 @@
+void main(){
+int noofweaks = 5;
+
+if(noofweaks==1){
+print("monday");
+
+}
+
+else if(noofweaks==2){
+print("tuesday");
+}
+
+else if(noofweaks==3){
+print("wednesday");
+}
+else if(noofweaks==4){
+print("tuersday");
+}
+
+else if(noofweaks==5){
+print("friday");
+}
+
+else{
+print("invalid");
+
+
+}
+
+
+
+}
